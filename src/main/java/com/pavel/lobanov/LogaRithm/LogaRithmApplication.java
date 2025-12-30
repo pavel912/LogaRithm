db0c4dd5dba4f8cb723a93d829f754d142e71e44
Pavel Lobanov <pavel.lobanov@uni-konstanz.de>
2025-12-30 10:49:42 +0100

    add spring application

---
+++ src/main/java/com/pavel/lobanov/LogaRithm/LogaRithmApplication.java
@@ -0,0 +1,13 @@
+package com.pavel.lobanov.LogaRithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogaRithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogaRithmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pavel/lobanov/LogaRithm/LogaRithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavel.lobanov.LogaRithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogaRithmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
